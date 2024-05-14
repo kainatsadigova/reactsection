13a447b7075a92292677e80550d551903529cb62
Class Time <mirzoevaxedice0@gmai.com>
2024-05-14 10:21:57 +0400

    creat

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <nav className='navbar'>
+        <div><h1>Accupay</h1></div>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Shop</li>
+            <li>Blog</li>
+            <li>Contact</li>
+            <li>Pages</li>
+          </ul>
+        </div>
+        <div className='tel'>+ 1234 567 865</div>
+        <div className='btn'><button>Get Started</button></div>
+      </nav>
+    </div>
+  )
+}
